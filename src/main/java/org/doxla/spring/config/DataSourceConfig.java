dd1d2f08a8b97ec82976c7fbab1234b07688e411
Dan Oxlade <dan.oxlade@gmail.com>
2011-12-16 18:40:18 +0000

    using component scanning

--- src/main/java/org/doxla/spring/config/DataSourceConfig.java
+++ src/main/java/org/doxla/spring/config/DataSourceConfig.java
@@ -4,10 +4,12 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Configuration
+@Profile("prod")
 public class DataSourceConfig {
 
     @Value("${database.driver.classname}")
